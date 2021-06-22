0e3e0f37264f0104c5504cfdd5914d782126a2b4
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-06-22 22:42:45 +0900

    ユーザー一覧取得の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,32 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
+import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const { loading, getUsers, users } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="test"
-          fullName="test test"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h={"100vh"}>
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
